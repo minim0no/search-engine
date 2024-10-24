54c0f2b67d44a56d8d0c54b19e035b772a19bbaa
minim0no <ahmedlol445@tamu.edu>
2024-10-24 18:47:02 -0500

    Add logo and search bar

--- search-engine-web-client/src/App.js
+++ search-engine-web-client/src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./Home";
-import Search from "./Search";
 
 function App() {
     return (
-        <>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </>
+        <div className="App w-full h-full">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
 
--- search-engine-web-client/src/Home.js
+++ search-engine-web-client/src/Home.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
 export default function Home() {
+    const [query, setQuery] = useState("");
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        setQuery(event.target.value);
+    };
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="w-full h-full flex flex-col gap-20 justify-center items-center">
+            {/* <img src="/heb-logo.png" alt="logo" className="w-[400px]" /> */}
+            <div className="flex justify-center items-center gap-6 bg-primary p-5 rounded-full">
+                <select className="py-2 pl-3 rounded-full text-primary font-bold">
+                    <option value="fruit-vegetable">Fruits & Vegetables</option>
+                    <option value="bakery">Bakery</option>
+                    <option value="home-outdoor">Home & Outdoor</option>
+                    <option value="health">Health</option>
+                </select>
+
+                <input
+                    type="text"
+                    placeholder="Search for products"
+                    value={query}
+                    onChange={handleChange}
+                    className="py-2 pl-3 rounded-full text-primary placeholder-primary placeholder-opacity-30 font-bold w-[600px]"
+                />
+            </div>
         </div>
     );
 }
